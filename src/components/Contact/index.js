d6facb71551956107810eb654a8b17b20bd7e0ed
mallynnk <mallory.korpics@gmail.com>
2020-10-15 14:43:22 -0500

    validated form data

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-
+import { validateEmail } from '../../utils/helpers';
 
 
 function ContactForm() {
@@ -7,10 +7,33 @@ function ContactForm() {
 
     const [formState, setFormState] = useState({ name: '', email: '', message: '' });
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     const { name, email, message } = formState;
 
     function handleChange(e) {
-        setFormState({...formState, [e.target.name]: e.target.value })
+        if (e.target.name === 'email') {
+            const isValid = validateEmail(e.target.value);
+            console.log(isValid);
+            // isValid conditional statement
+            if (!isValid) {
+                setErrorMessage('Your email is invalid.');
+              } else {
+                setErrorMessage('');
+              }
+              
+          } else {
+            if (!e.target.value.length) {
+              setErrorMessage(`${e.target.name} is required.`);
+            } else {
+              setErrorMessage('');
+            }
+          }
+
+          if (!errorMessage) {
+            setFormState({ ...formState, [e.target.name]: e.target.value });
+          }
+        console.log('errorMessage', errorMessage)
       }
 
     function handleSubmit(e) {

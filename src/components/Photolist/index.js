74f1b89fadebcab6849aa5c75c578f7293447327
mallynnk <mallory.korpics@gmail.com>
2020-10-20 10:43:43 -0500

    added Modal Component with tests

--- src/components/Photolist/index.js
+++ src/components/Photolist/index.js
@@ -1,8 +1,11 @@
 import React, { useState } from 'react';
 import Modal from '../Modal';
+
 // import Modal from '../Modal';
 
 const Photolist = ({ category }) => {
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   const [photos] = useState([
     {
@@ -103,27 +106,31 @@ const Photolist = ({ category }) => {
   ]);
 
   const currentPhotos = photos.filter((photo) => photo.category === category);
+  const [currentPhoto, setCurrentPhoto] = useState();
 
   const toggleModal = (image, i) => {
-    // current photo
+    setCurrentPhoto({...image, index: i})
+      setIsModalOpen(!isModalOpen);
   }
-  
+
   return (
     <div>
-      <Modal />
-      <div className="flex-row">
-        {currentPhotos.map((image, i) => (
-          <img
-          src={require(`../../assets/small/${category}/${i}.jpg`)}
-          alt={image.name}
-          className="img-thumbnail mx-1"
-          onClick={() => toggleModal(image, i)}
-          key={image.name}
-        />
-        ))}
-      </div>
+        <div className="flex-row">
+            {isModalOpen && (
+                <Modal currentPhoto={currentPhoto} onClose={toggleModal} />
+            )}                {currentPhotos.map((image, i) => (
+                <img
+                    src={require(`../../assets/small/${category}/${i}.jpg`)}
+                    alt={image.name}
+                    className="img-thumbnail mx-1"
+                    onClick={() => toggleModal(image, i)}
+                    key={image.name}
+                />
+            ))}
+        </div>
     </div>
-  );
+);
 };
+
 
 export default Photolist;
